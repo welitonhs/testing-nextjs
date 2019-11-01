0859c2d31cdb70919b69bb0129fc2791b66e17d6
welitonhs <weliton.sernajotto23@gmail.com>
2019-11-01 18:09:56 -0300

    Third Leason - Using Shared Components

---
+++ 3-using-shared-components/pages/about.js
@@ -0,0 +1,10 @@
+import Header from '../src/comps/Header';
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <p>This is the about page</p>
+    </div>
+  )
+}
---
+++ 3-using-shared-components/pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../src/comps/Header';
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <p>Hello Next.js</p>
+    </div>
+  );
+}
