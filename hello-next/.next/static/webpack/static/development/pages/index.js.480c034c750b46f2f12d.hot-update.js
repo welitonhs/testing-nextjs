485c7c7506b69321b6ed3189bf39e38f09d832af
welitonhs <weliton.sernajotto23@gmail.com>
2019-11-01 16:35:11 -0300

    First Leason

---
+++ hello-next/.next/static/webpack/static/development/pages/index.js.480c034c750b46f2f12d.hot-update.js
@@ -0,0 +1,37 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\Suporte\\Desktop\\Learning\\next-js\\hello-next\\pages\\index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Index = function Index() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Hello Next.js"));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.480c034c750b46f2f12d.hot-update.js.map
